2b593e5028f5b0ba4f4d6544ee522ea74f3e29f8
mrus1 <russ.matija@gmail.com>
2020-01-08 20:57:29 +0100

    develope logic for saveing display name in localStorage

--- application.py
+++ application.py
@@ -11,7 +11,3 @@ socketio = SocketIO(app)
 @app.route("/")
 def index():
     return render_template("index.html")
-
-
-if __name__ == "__main__":
-    app.run()
--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,21 @@
-alert("Choose your username")
+document.addEventListener('DOMContentLoaded', () => {
+  // Set display_name
+  if (!localStorage.getItem('display_name')) {
+    console.log("display_name is not set")
+    document.getElementById('noDisplayName').style.display = "block";
+
+    // Get new display_name
+    document.querySelector('#setDisplayName').onsubmit = () => {
+      const display_name = document.querySelector('#displayName').value;
+      if ( display_name != '')
+        localStorage.setItem('display_name', display_name)
+      else
+        alert("empty name not allowed")
+    }
+
+  } else {
+    console.log('display name exists')
+    document.getElementById('enterRooms').style.display = "block";
+    document.querySelector('#user').innerHTML = localStorage.getItem('display_name');
+  }
+});
