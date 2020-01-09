1553123e9a70ca4918b3329360c2cacc5a3d22be
mrus1 <russ.matija@gmail.com>
2020-01-09 23:15:28 +0100

    enabel create new channels

--- application.py
+++ application.py
@@ -7,7 +7,19 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+channels_list = {0: "default"}
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    print("========== channels list ============")
+    print(channels_list)
+    return render_template("index.html", channels_list=channels_list)
+
+@socketio.on("submit channel")
+def channel(data):
+    print(data)
+    count = len(channels_list)
+    channel_new = data["channel-name"]
+    channels_list[count] = channel_new
+    print(channels_list)
+    emit("channel name", channels_list, broadcast=True)
--- static/js/index.js
+++ static/js/index.js
@@ -9,7 +9,62 @@ if (!localStorage.getItem('display_name')) {
 
 // Set display name globaly
 document.addEventListener("DOMContentLoaded", () => {
+
+  // Connect to websocket
+  var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
   const display_name = localStorage.getItem('display_name')
 
   document.querySelector('#display_name').innerHTML = display_name;
+
+  // When connected Create channel
+  socket.on('connect', () => {
+
+    // By default, submit button is disabled
+    document.querySelector('#submit').disabled = true;
+
+    // Enable button only if there is text in the input field
+    document.querySelector('#get-channel').onkeyup = () => {
+      if (document.querySelector('#get-channel').value.length > 0) {
+        document.querySelector('#submit').disabled = false;
+      } else {
+        document.querySelector('#submit').disabled = true;
+      }
+    };
+
+
+    document.querySelector('#new-channel').onsubmit = () => {
+      // TODO: If channel name already exists
+
+      // New wanted channel name
+      const newName = document.querySelector('#get-channel').value;
+
+      // Clear input field and disable button again
+      document.querySelector('#get-channel').value = '';
+      document.querySelector('#submit').disabled = true;
+
+      socket.emit('submit channel', {'channel-name': newName});
+
+      // Stop form from submitting
+      return false;
+    };
+  });
+
+  // When a new channel is created, add it to list of channels
+  socket.on('channel name', data => {
+    const last = Object.values(data).slice(-1)
+
+    if ( Object.values(data).slice(0, -1).find(e => e == last) ) {
+      return 0;
+    } else {
+      // Create new chanel to show on page
+      const li = document.createElement('li');
+      li.innerHTML = last;
+
+      // Add new channel to channels list
+      document.querySelector('#channels').append(li);
+    }
+
+  });
+
 });
