508f399fb430a62c489883ef651ebb698e914256
mrus1 <russ.matija@gmail.com>
2020-01-15 00:26:56 +0100

    make chacking for existing channel names work

--- application.py
+++ application.py
@@ -9,17 +9,22 @@ socketio = SocketIO(app)
 
 channels_list = {0: "default"}
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    print("========== channels list ============")
-    print(channels_list)
+    print("template reload")
     return render_template("index.html", channels_list=channels_list)
 
-@socketio.on("submit channel")
-def channel(data):
-    print(data)
+@socketio.on('submit channel')
+def handle_channels(data):
     count = len(channels_list)
     channel_new = data["channel-name"]
-    channels_list[count] = channel_new
-    print(channels_list)
-    emit("channel name", channels_list, broadcast=True)
+
+    if channel_new in list(channels_list.values()):
+        emit("channel name", False, broadcast=True)
+    else:
+        channels_list[count] = channel_new
+        emit("channel name", channels_list, broadcast=True)
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,4 @@
-// Prompt when user first visits
+// Prompt when user first visits site
 if (!localStorage.getItem('display_name')) {
   let getName = window.prompt("Enter your nickname:")
   while (getName == null || getName == "" || !getName.match(/^[0-9a-zA-Z]+$/)){
@@ -7,64 +7,67 @@ if (!localStorage.getItem('display_name')) {
   localStorage.setItem('display_name', getName)
 };
 
-// Set display name globaly
+
 document.addEventListener("DOMContentLoaded", () => {
 
   // Connect to websocket
   var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 
+  // Variables
   const display_name = localStorage.getItem('display_name')
+  const errorMessage = document.querySelector("#error1")
+  const submitButton = document.querySelector("#submit")
+  const getChannel = document.querySelector("#get-channel")
+  let error1 = false;
 
+  // Show chosen display name on page
   document.querySelector('#display_name').innerHTML = display_name;
 
-  // When connected Create channel
-  socket.on('connect', () => {
+  // By default set submit button to disabled and errorMessage to none
+  submitButton.disabled = true;
+  errorMessage.style.display = "none"
 
-    // By default, submit button is disabled
-    document.querySelector('#submit').disabled = true;
+  // Enable button only if there is text in the input field
+  getChannel.onkeyup = () => {
 
-    // Enable button only if there is text in the input field
-    document.querySelector('#get-channel').onkeyup = () => {
-      if (document.querySelector('#get-channel').value.length > 0) {
-        document.querySelector('#submit').disabled = false;
-      } else {
-        document.querySelector('#submit').disabled = true;
-      }
-    };
+    // Set submit button
+    getChannel.value.length > 0 ? submitButton.disabled = false : submitButton.disabled = true;
 
+    // Show error message if channel name already exists
+    error1 ? errorMessage.style.display = "block" : errorMessage.style.display = "none";
 
-    document.querySelector('#new-channel').onsubmit = () => {
-      // TODO: If channel name already exists
+  };
 
-      // New wanted channel name
-      const newName = document.querySelector('#get-channel').value;
+  document.querySelector('#new-channel').onsubmit = () => {
 
-      // Clear input field and disable button again
-      document.querySelector('#get-channel').value = '';
-      document.querySelector('#submit').disabled = true;
+    // New wanted channel name
+    const newName = getChannel.value;
 
-      socket.emit('submit channel', {'channel-name': newName});
+    // Clear input field and disable button again
+    getChannel.value = '';
+    submitButton.disabled = true;
 
-      // Stop form from submitting
-      return false;
-    };
-  });
+    socket.emit('submit channel', {'channel-name': newName})
+
+    // Stop form from submitting
+    return false;
+  };
 
   // When a new channel is created, add it to list of channels
   socket.on('channel name', data => {
-    const last = Object.values(data).slice(-1)
+    if (data != false) {
+      const last = Object.values(data).slice(-1)
 
-    if ( Object.values(data).slice(0, -1).find(e => e == last) ) {
-      return 0;
-    } else {
       // Create new chanel to show on page
       const li = document.createElement('li');
       li.innerHTML = last;
 
       // Add new channel to channels list
       document.querySelector('#channels').append(li);
+      error1 = false;
+    } else {
+      error1 = true;
     }
-
   });
 
 });
